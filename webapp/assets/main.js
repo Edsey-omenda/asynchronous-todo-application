d6d5a6ac2fd1ef62c647f77217b036b18d865ff0
Edsey omenda <omendajames0317@gmail.com>
2022-07-01 21:14:19 +0300

    completed assignment

--- webapp/assets/main.js
+++ webapp/assets/main.js
@@ -1,3 +1,4 @@
+
 const addNewTodoButton = document.getElementById("add-new-todo");
 const createTodoButton = document.getElementById("create-todo");
 const updateTodoButton = document.getElementById("update-todo");
@@ -11,68 +12,47 @@ updateTodoButton.style.display = "none"
 
 let Todos = [];
 
-// REST api
-
 const displayAllTodos = () => {
 	todoList.innerHTML = ""
-	axios.get("http://localhost:3000/posts").then(res => {	
-		Todos = [...res.data]
-		if (Todos.length == 0) {
 
+	if (Todos.length == 0) {
+
+		todoList.innerHTML += `
+		<div class = "empty-todo">
+		<img src="undraw_empty_xct9.png" alt="empty image" style="width: 50%;">
+		<br>
+		<span style="font-family: 'Fira Sans', sans-serif; font-size: 20px; font-weight: bold;">There are no todos yet...</span>
+		<br>
+		</div>
+		`;
+	} else {		
+		for(let key in Todos){
+			let todo = Todos[key];
 			todoList.innerHTML += `
-			<div class = "empty-todo">
-			<img src="./assets/images/undraw_empty_xct9.png" alt="empty image" style="width: 50%;">
-			<br>
-			<span style="font-family: 'Fira Sans', sans-serif; font-size: 20px; font-weight: bold;">There are no todos yet...</span>
-			<br>
+			<div data-id="${todo.id}" class="todo-content-item">
+				<span class="todo-id">▪️ ${todo.id} ▪️</span>
+				${todo.status === "Complete" ? `<span style="text-decoration: line-through;" class="todo-text">${todo.body}</span>` : `<span class="todo-text">${todo.body}</span>`}
+				<span class="todo-date">Created at : ${todo.timestamp}</span>
+				${todo.status === "Complete" ? `<span class="todo-status complete">▪️ ${todo.status} ▪️</span>` : `<span class="todo-status incomplete">▪️ ${todo.status} ▪️</span>`}
+				<div style="display: flex; flex-direction: column; justify-content: space-around; align-items: center;" class="actions-window">
+					<i class="far fa-edit"></i>
+					<i class="far fa-trash-alt"></i>
+					${todo.status === "Complete" ? "" : '<i class="fas fa-check"></i>'}
+				</div>
 			</div>
 			`;
-		} else {		
-			for(let key in Todos){
-				let todo = Todos[key];
-				todoList.innerHTML += `
-				<div data-id="${todo.id}" class="todo-content-item">
-					<span class="todo-id">▪️ ${todo.id} ▪️</span>
-					${todo.status === "Complete" ? `<span style="text-decoration: line-through;" class="todo-text">${todo.body}</span>` : `<span class="todo-text">${todo.body}</span>`}
-					<span class="todo-date">Created at : ${todo.timestamp}</span>
-					${todo.status === "Complete" ? `<span class="todo-status complete">▪️ ${todo.status} ▪️</span>` : `<span class="todo-status incomplete">▪️ ${todo.status} ▪️</span>`}
-					<div style="display: flex; flex-direction: column; justify-content: space-around; align-items: center;" class="actions-window">
-						<i class="far fa-edit"></i>
-						<i class="far fa-trash-alt"></i>
-						${todo.status === "Complete" ? "" : '<i class="fas fa-check"></i>'}
-					</div>
-				</div>
-				`;
-			};
-		}
-	}).catch(err => console.log(err));
-
-	console.log(Todos);
+		};
+	}
 }
 
 displayAllTodos();
-
-// const addTodo = () => {
-// 	const id = idField.value;
-// 	const timestamp = timeField.value;
-// 	const body = bodyField.value;
-// 	const status = "Not complete";
-// 	Todos.push({id,timestamp, body, status})
-// 	idField.value = "";
-// 	timeField.value = "";
-// 	bodyField.value = "";
-// 	displayAllTodos();
-// }
 
 const addTodo = () => {
 	const id = idField.value;
 	const timestamp = timeField.value;
 	const body = bodyField.value;
 	const status = "Not complete";
-	axios
-    .post('http://localhost:3000/posts', {id,timestamp, body, status})
-    .then(res => console.log(res.data))
-    .catch(err => console.error(err));
+	Todos.push({id,timestamp, body, status})
 	idField.value = "";
 	timeField.value = "";
 	bodyField.value = "";
@@ -90,7 +70,7 @@ const editTodo = (itemId) => {
 }
 
 const generateID = () => {
-	let id = `${Math.random().toString(36).substr(2, 6)}-${Math.random().toString(36).substr(2, 4)}-${Math.random().toString(36).substr(2, 4)}-${Math.random().toString(36).substr(2, 6)}`;
+	let id = `${Math.random().toString(36).substring(2, 6)}-${Math.random().toString(36).substring(2, 4)}-${Math.random().toString(36).substr(2, 4)}-${Math.random().toString(36).substr(2, 6)}`;
 	return id;
 }
 
